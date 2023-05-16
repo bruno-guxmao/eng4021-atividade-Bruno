56c126ef93bca4975f5f384c48eebe1a34585ad4
bruno_gusmao <52937908+bruno-guxmao@users.noreply.github.com>
2023-05-15 19:22:59 -0700

    Update models.py

--- appdobruno/models.py
+++ appdobruno/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-class Task(models.Model):
-  title = models.CharField(max_length = 50)
-  description = models.TextField()
-  due_date = models.DateField()
-  done = models.BooleanField()
+class Task(models.Model): #Define a classe Task que herda da classe Model 
+  
+  title = models.CharField(max_length = 50) # Define um campo de caracteres (string) que possui no máximo 50 caracteres
+  
+  description = models.TextField() # Define um campo de texto para armazenar uma descrição mais longa
+  
+  due_date = models.DateField() # Define um campo de data para armazenar a data de vencimento da tarefa
+  
+  done = models.BooleanField() # Define um campo booleano para indicar se a tarefa foi concluída (True) ou não (False)
